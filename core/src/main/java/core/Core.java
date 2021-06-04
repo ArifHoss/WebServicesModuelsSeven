394eb090e3e9f8aec085407a4891954aac0b8374
Arif <hossaininfo@ymail.com>
2021-06-04 18:10:38 +0200

    ExecutorService created Thread handled

--- core/src/main/java/core/Core.java
+++ core/src/main/java/core/Core.java
@@ -5,42 +5,72 @@ import utils.Utils;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Core {
 
     public static void main(String[] args) {
 
+        ExecutorService executorService = Executors.newCachedThreadPool();
+
         try (ServerSocket serverSocket = new ServerSocket(5050)) {
 
             while (true) {
                 Socket client = serverSocket.accept();
-                System.out.println("ClientIpAddress: " + client.getInetAddress());
+                //starta tråd/Start thread
 
-                var inputFromClient = new BufferedReader(new InputStreamReader(client.getInputStream()));
-                while (true) {
-                    String line = inputFromClient.readLine();
-                    if (line == null||line.isEmpty()) {
-                        break;
-                    }
-                    System.out.println(line);
+                executorService.submit(() -> haldleConnection(client));
 
-                }
-                PrintWriter outPutToClient = new PrintWriter(client.getOutputStream());
-                outPutToClient.print("HTTP/1.1 404 Not Found\r\nContent-length: 0\r\n\r\n");
+//                Thread thread = new Thread(() -> haldleConnection(client));
+//                thread.start();
+//                Thread thread = new Thread(){
+//                    @Override
+//                    public void run() {
+//                        System.out.println(this.getClass().getName());
+//                        System.out.println(Thread.currentThread().getName());
+//                        try {
+//                            haldleConnection(client);
+//                        } catch (IOException e) {
+//                            e.printStackTrace();
+//                        }
+//                    }
+//                };
+//                thread.start();
 
-
-//                System.out.println(inputFromClient.readLine());
-//                inputFromClient.lines().forEach(System.out::println);
-
-                outPutToClient.flush();
-                outPutToClient.close();
-                inputFromClient.close();
-                client.close();
+//                haldleConnection(client);
             }
 
         } catch (IOException e) {
             e.printStackTrace();
         }
 
+    }
+
+    private static void haldleConnection(Socket client) {
+        try {
+            System.out.println(client.getInetAddress());
+            System.out.println(Thread.currentThread().getName());
+
+            var inputFromClient = new BufferedReader(new InputStreamReader(client.getInputStream()));
+            while (true) {
+                String line = inputFromClient.readLine();
+                if (line == null || line.isEmpty()) {
+                    break;
+                }
+                System.out.println(line);
+
+            }
+            PrintWriter outPutToClient = new PrintWriter(client.getOutputStream());
+            outPutToClient.print("HTTP/1.1 404 Not Found\r\nContent-length: 0\r\n\r\n");
+
+            outPutToClient.flush();
+            outPutToClient.close();
+            inputFromClient.close();
+            client.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
